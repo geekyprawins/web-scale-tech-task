a27d10485e471f34f251b22feb4304d1494fbf5b
geekyprawins <swamiraju.varma@learner.manipal.edu>
2023-02-05 10:27:05 +0530

    functionality done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:convert';
-
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -12,6 +12,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseService.getWatchLaterLaunches();
   runApp(const MyApp());
 }
 
@@ -39,6 +40,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  PageController? _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    getLaunchData();
+  }
+
+  @override
+  void dispose() {
+    _pageController!.dispose();
+    super.dispose();
+  }
 
   List<LaunchData> upcmngLaunches = [];
 
@@ -57,13 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       print("Fetched ${upcmngLaunches.length} launches!");
     });
-
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getLaunchData();
   }
 
   @override
@@ -72,30 +81,166 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("SpaceX Upcoming Launches"),
       ),
-      body: Center(
-        child:
-        (upcmngLaunches.isEmpty) ?
-        const CircularProgressIndicator() :
-        ListView.builder(itemBuilder: (context, index) {
-          final LaunchData launchData = upcmngLaunches[index];
-          final String name = launchData.missionName;
-          final String flightNo = launchData.flightNumber.toString();
-          return ListTile(title: Text(name),
-            subtitle: Text(flightNo),
-          leading: Text(launchData.rocket.rocketName),
-          trailing: IconButton(onPressed: () {
-            //TODO: add to db
-            FirebaseService.addLaunchDataToCollection(launchData);
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Added to Watch Later"),),);
-          }, icon: const Icon(
-            Icons.watch_later_outlined
-          ),
-          ),);
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() => _currentIndex = index);
         },
-        itemCount: upcmngLaunches.length,
-        ),
+        children: <Widget>[
+          UpcomingLaunches(upcmngLaunches: upcmngLaunches),
+          WatchLaterLaunches(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController!.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(title: Text('All'), icon: Icon(Icons.home)),
+          BottomNavyBarItem(
+              title: Text('Watch Later'),
+              icon: Icon(Icons.watch_later_outlined)),
+        ],
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class UpcomingLaunches extends StatelessWidget {
+  const UpcomingLaunches({
+    super.key,
+    required this.upcmngLaunches,
+  });
+
+  final List<LaunchData> upcmngLaunches;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: (upcmngLaunches.isEmpty)
+          ? const CircularProgressIndicator()
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                final LaunchData launchData = upcmngLaunches[index];
+                final String missionName = launchData.missionName;
+                final String flightNo = launchData.flightNumber.toString();
+                return Material(
+                  elevation: 1.5,
+                  color: Colors.tealAccent,
+                  child: ListTile(
+                    title: Text(missionName),
+                    subtitle: Text(flightNo),
+                    leading: Text(launchData.rocket.rocketName),
+                    trailing: IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Add to watch later?'),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: const Text('Yes'),
+                                  onPressed: () {
+                                    FirebaseService.addLaunchDataToCollection(
+                                      launchData,
+                                    );
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text("Added to Watch Later"),
+                                      ),
+                                    );
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                TextButton(
+                                  child: const Text('No'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      icon: const Icon(Icons.watch_later_outlined),
+                    ),
+                  ),
+                );
+              },
+              itemCount: upcmngLaunches.length,
+            ),
+    );
+  }
+}
+
+class WatchLaterLaunches extends StatefulWidget {
+  const WatchLaterLaunches({Key? key}) : super(key: key);
+
+  @override
+  State<WatchLaterLaunches> createState() => _WatchLaterLaunchesState();
+}
+
+class _WatchLaterLaunchesState extends State<WatchLaterLaunches> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseService.getWatchLaterStream(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            final wlData = LaunchData.fromJson(data);
+            return ListTile(
+              title: Text(wlData.missionName),
+              subtitle: Text(wlData.flightNumber.toString()),
+              trailing: IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Remove from watch later?'),
+                        actions: <Widget>[
+                          TextButton(
+                            child: const Text('Yes'),
+                            onPressed: () {
+                              FirebaseService.deleteFromWatchLater(wlData);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Removed from Watch Later"),
+                                ),
+                              );
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: const Text('No'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                icon: const Icon(Icons.delete),
+              ),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,13 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:webscaletech_task/models/launch_data.dart';
 
-class FirebaseService{
-
+class FirebaseService {
 
   static void addLaunchDataToCollection(LaunchData launchData) async {
-    var launchDataCollection = FirebaseFirestore.instance.collection("launches");
-    await launchDataCollection.add(launchData.toJson()).then((value) => print("Added to Firestore!"));
+    var launchDataCollection =
+        FirebaseFirestore.instance.collection("launches");
+    await launchDataCollection.doc(launchData.missionName)
+        .set(launchData.toJson())
+        .then((value) => print("Added to Firestore!"));
   }
 
+  static Future<List<LaunchData>> getWatchLaterLaunches() async {
+    List<LaunchData> watchLaterData = [];
+    var launchDataCollection =
+        FirebaseFirestore.instance.collection("launches");
+    await launchDataCollection.get().then((querySnapshots) {
+      for (var qs in querySnapshots.docs) {
+        LaunchData launchData = LaunchData.fromJson(qs.data());
+        print("Fetched watch later: ${launchData.missionName}");
+        watchLaterData.add(launchData);
+      }
+    });
+    return watchLaterData.toList();
+  }
 
+  static Stream<QuerySnapshot> getWatchLaterStream()  {
+    final Stream<QuerySnapshot> wlStream = FirebaseFirestore.instance.collection('launches').snapshots();
+    return wlStream;
+  }
+
+  static void deleteFromWatchLater(LaunchData ld)  {
+    var launchDataCollection =
+    FirebaseFirestore.instance.collection("launches");
+    launchDataCollection.doc(ld.missionName).delete();
+  }
 }
